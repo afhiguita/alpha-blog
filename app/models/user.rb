825b51cf8e889b611ccea10905551a478ea0d713
Adrian Higuita <adrian.higuita@hogarurbano.com>
2018-08-13 20:34:49 -0300

    Validation Username and Email from User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ApplicationRecord
-  def create
+  validates :username, presence: true, uniqueness: { case_sensitive: :false },  length: { minimum: 6, maximum: 30}
 
-  end
-  def edit
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 200 }, uniqueness: {case_sensitive: true}, format: {with: VALID_EMAIL_REGEX}
 
-  end
-  def destroy
-
-  end
 end
